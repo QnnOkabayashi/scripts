b2ea6e369affcdadebd0c7eb694bc58af1cceef8
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-21 10:46:00 -0800

    Removed redundency

--- PiDiffuserCam/setup.sh
+++ PiDiffuserCam/setup.sh
@@ -1,12 +1,12 @@
 echo "Beginning PiDiffuserCam installation..."
 
 echo "Updating..."
-sudo apt-get update &>/dev/null
+sudo apt-get update
 
 echo "Installing Git and PiCamera..."
-sudo apt-get install git python3-picamera &>/dev/null
+sudo apt-get install git python3-picamera
 
 echo "Cloning project repo..."
-git clone https://github.com/QnnOkabayashi/PiDiffuserCam.git /home/$(whoami)/PiDiffuserCam &>/dev/null
+git clone https://github.com/QnnOkabayashi/PiDiffuserCam.git /home/$(whoami)/PiDiffuserCam
 
 echo "Finished PiDiffuserCam installation."
