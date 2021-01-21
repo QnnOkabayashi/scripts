8c5709bdb5db9c572239a927c34305d1e786a6fc
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-20 21:43:54 -0800

    Added progress messages

--- PiDiffuserCam/setup.sh
+++ PiDiffuserCam/setup.sh
@@ -1,8 +1,12 @@
 echo "Beginning PiDiffuserCam installation..."
 
+echo "Updating..."
 sudo apt-get update
+
+echo "Installing Git and PiCamera..."
 sudo apt-get install git python3-picamera
 
+echo "Cloning project repo..."
 git clone https://github.com/QnnOkabayashi/PiDiffuserCam.git /home/$(whoami)/PiDiffuserCam
 
 echo "Finished PiDiffuserCam installation."
