04cd98dfc80af50902462c98d95f8cd839d0b47c
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-20 21:23:20 -0800

    Added setup for PiDiffuserCam

---
+++ PiDiffuserCam/setup.sh
@@ -0,0 +1,8 @@
+# echo Setting up PiDiffuserCam...
+
+# sudo apt-get update
+# sudo apt-get install git python3-picamera
+
+# git clone https://github.com/QnnOkabayashi/PiDiffuserCam.git
+
+echo "Testing remotely running a script!"
