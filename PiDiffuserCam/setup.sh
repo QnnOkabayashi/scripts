2136c50cb244d293ffe7661111333003d5aac10c
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-21 10:44:26 -0800

    Silenced output

--- PiDiffuserCam/setup.sh
+++ PiDiffuserCam/setup.sh
@@ -1,12 +1,12 @@
 echo "Beginning PiDiffuserCam installation..."
 
 echo "Updating..."
-sudo apt-get update
+sudo apt-get update &>/dev/null
 
 echo "Installing Git and PiCamera..."
-sudo apt-get install git python3-picamera
+sudo apt-get install git python3-picamera &>/dev/null
 
 echo "Cloning project repo..."
-git clone https://github.com/QnnOkabayashi/PiDiffuserCam.git /home/$(whoami)/PiDiffuserCam
+git clone https://github.com/QnnOkabayashi/PiDiffuserCam.git /home/$(whoami)/PiDiffuserCam &>/dev/null
 
 echo "Finished PiDiffuserCam installation."
