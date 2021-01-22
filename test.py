d574646304fb3705bdff9474a8ddda1b25bbce6b
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-21 20:00:21 -0800

    updated test

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-if __name__ == '__main__':
-    print('Success!')
+if __name__ == '<string>':
+    print("I'm being executed as a string!")
+elif __name__ == '__main__':
+    print("I am main")
 else:
     print("Failure :(")
