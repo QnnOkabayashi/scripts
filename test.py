2eee717036486249587365dd8c4b2e51d9299253
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-21 20:55:23 -0800

    Removed redundent files

--- test.py
+++
@@ -1,7 +0,0 @@
-# if __name__ == '<string>':
-#     print("I'm being executed as a string!")
-# elif __name__ == '__main__':
-#     print("I am main")
-# else:
-#     print("Failure :(")
-print(__name__)
