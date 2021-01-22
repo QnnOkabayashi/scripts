ebebfe2b0098becac3ced73688caac74d9385a4f
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-01-21 20:03:10 -0800

    testing __name__

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-if __name__ == '<string>':
-    print("I'm being executed as a string!")
-elif __name__ == '__main__':
-    print("I am main")
-else:
-    print("Failure :(")
+# if __name__ == '<string>':
+#     print("I'm being executed as a string!")
+# elif __name__ == '__main__':
+#     print("I am main")
+# else:
+#     print("Failure :(")
+print(__name__)
